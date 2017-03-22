6eb15c98216b8cd2b4397f321798eebb2d0f29b6
Liu Yihao <liuyh615@126.com>
2017-03-23 01:09:08 +0800

    add a config item: domain

--- controller/account.py
+++ controller/account.py
@@ -11,14 +11,15 @@ import json
 from util.env import *
 
 env = get_env()
+base_url = env['domain'] and env['domain'] or 'http://localhost'
 if env['port']:
-    base_url='localhost:'+str(env['port'])
-else:
-    base_url='wuchg.top:8080'
+    base_url += ':' + str(env['port'])
+
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
         return self.get_secure_cookie("username")
+
     def is_viewed(self, pid):
         viewd = self.get_secure_cookie("vd").decode('UTF-8').split(',')
         if str(pid) in viewd:
@@ -29,36 +30,36 @@ class BaseHandler(tornado.web.RequestHandler):
         return False
 
 
-
 class LoginHandler(BaseHandler):
     def get(self):
         next = self.get_argument('next')
         self.render('login.html', next=next)
 
-    # def post(self):
-    #     uid = self.get_argument('username')
-    #     next = self.get_argument('next')
-    #     #self.get_argument('s')
-    #
-    #     if not uid.isdigit():
-    #         self.redirect(next)
-    #     else:
-    #         user = userDB(uid)
-    #         pwd = self.get_argument('password')
-    #         # salt = binascii.hexlify(os.urandom(20)).decode()
-    #         res = user.check(pwd)
-    #
-    #         if res:
-    #             u_name = user.query()['u_name']
-    #             self.set_secure_cookie("u_name", u_name)
-    #             self.set_secure_cookie("username", str(uid))
-    #             self.set_secure_cookie("vd", '')
-    #         self.redirect(next)
+        # def post(self):
+        #     uid = self.get_argument('username')
+        #     next = self.get_argument('next')
+        #     #self.get_argument('s')
+        #
+        #     if not uid.isdigit():
+        #         self.redirect(next)
+        #     else:
+        #         user = userDB(uid)
+        #         pwd = self.get_argument('password')
+        #         # salt = binascii.hexlify(os.urandom(20)).decode()
+        #         res = user.check(pwd)
+        #
+        #         if res:
+        #             u_name = user.query()['u_name']
+        #             self.set_secure_cookie("u_name", u_name)
+        #             self.set_secure_cookie("username", str(uid))
+        #             self.set_secure_cookie("vd", '')
+        #         self.redirect(next)
 
 
 class UserRegisterHandler(BaseHandler):
     def get(self):
         self.render('register.html')
+
     def post(self):
         major = self.get_argument('major')
         phone = self.get_argument('phone')
@@ -75,11 +76,13 @@ class UserRegisterHandler(BaseHandler):
         self.set_secure_cookie("vd", '')
         self.redirect('/')
 
+
 class LogoutHandler(BaseHandler):
     def get(self):
         self.clear_cookie("username")
         self.clear_cookie("u_name")
         self.redirect("/")
+
     def post(self):
         if (self.get_argument("logout", None)):
             self.clear_cookie("username")
@@ -95,6 +98,7 @@ class optionHandler(BaseHandler):
         role = data['role']
         self.render("dashboard.html", uid=uid, u_name=self.get_secure_cookie('u_name'), data=data, role=role)
 
+
 class registedHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
@@ -105,10 +109,10 @@ class registedHandler(BaseHandler):
         u_name = user.u_name
         registed = res['registed'].split(',')
         statstr = []
-        projid=[]
+        projid = []
         stat = res['stat'].split(',')
         for i in range(3):
-            if registed[i]=='n':
+            if registed[i] == 'n':
                 registed[i] = 'None'
                 projid.append(0)
             else:
@@ -122,11 +126,12 @@ class registedHandler(BaseHandler):
             if int(stat[i]) == 2:
                 statstr.append('Succeed')
             stat[i] = int(stat[i])
-        if role=='admin':
+        if role == 'admin':
             self.render('403.html', u_name=u_name, role=role)
         else:
             self.render("registed.html", registed=registed, u_name=u_name,
-                    stat=stat, role=role, statstr=statstr, projid=projid)
+                        stat=stat, role=role, statstr=statstr, projid=projid)
+
 
 class forbiddenHandler(BaseHandler):
     @tornado.web.authenticated
@@ -135,6 +140,7 @@ class forbiddenHandler(BaseHandler):
         uid = int(tornado.escape.xhtml_escape(self.current_user))
         role = userDB(uid).query()['role']
         self.render('403.html', u_name=u_name, role=role)
+
 
 class memberHandler(BaseHandler):
     @tornado.web.authenticated
@@ -148,8 +154,8 @@ class memberHandler(BaseHandler):
 
 class jaloginHandler(BaseHandler):
     def get(self):
-
-        self.redirect("https://jaccount.sjtu.edu.cn/oauth2/authorize?response_type=code&client_id=htAZFp7DCwnd1A9q4yDwbyWX&redirect_uri=http://"+base_url+"/jacode")
+        self.redirect(
+            "https://jaccount.sjtu.edu.cn/oauth2/authorize?response_type=code&client_id=htAZFp7DCwnd1A9q4yDwbyWX&redirect_uri=" + base_url + "/jacode")
 
 
 class jacodeHandler(BaseHandler):
@@ -161,7 +167,7 @@ class jacodeHandler(BaseHandler):
             "client_secret": "9EADF115A76B760B16089708D06D6BF2EC08CE2C5E613D9B",
             "grant_type": "authorization_code",
             "code": code,
-            "redirect_uri": "http://"+base_url+"/jacode"
+            "redirect_uri": base_url + "/jacode"
         }).encode('utf-8')
         header = {
             "Host": "jaccount.sjtu.edu.cn",
@@ -176,7 +182,8 @@ class jacodeHandler(BaseHandler):
         res = respond.read().decode('utf-8')
         data = json.loads(res)
         access_token = data['access_token']
-        inform = urllib.request.urlopen('https://api.sjtu.edu.cn/v1/me/profile?access_token=%s' % access_token).read().decode('utf8')
+        inform = urllib.request.urlopen(
+            'https://api.sjtu.edu.cn/v1/me/profile?access_token=%s' % access_token).read().decode('utf8')
         inform = json.loads(inform)
         name = inform['entities'][0]['name']
         id = inform['entities'][0]['code']
